8e781b5c1251b0414786377da870700ffeff347f
Andi Alfian Bahtiar <55624363+anddfian@users.noreply.github.com>
2023-05-07 11:31:45 +0800

    [QR-007] Add Launcher Icon
    * Add Launcher Icon
    
    * Add Launcher Icon

--- app/src/main/java/com/bangkit/bisamerchant/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/bangkit/bisamerchant/ui/onboarding/OnboardingActivity.kt
@@ -1,12 +1,37 @@
 package com.bangkit.bisamerchant.ui.onboarding
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bangkit.bisamerchant.R
+import com.bangkit.bisamerchant.databinding.ActivityOnboardingBinding
+import com.bangkit.bisamerchant.ui.login.LoginActivity
 
-class OnboardingActivity : AppCompatActivity() {
+class OnboardingActivity : AppCompatActivity(), View.OnClickListener {
+    private var _binding: ActivityOnboardingBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        _binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGetStarted.setOnClickListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_get_started -> {
+                val intent = Intent(this@OnboardingActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
