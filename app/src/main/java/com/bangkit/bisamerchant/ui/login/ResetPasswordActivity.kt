770e32dba139e923e3846c93fa1595b2292bbf32
tegarfn <tegarfitrah.na@gmail.com>
2023-05-09 15:28:31 +0800

    Create Forgot Password Screen - UI

---
+++ app/src/main/java/com/bangkit/bisamerchant/ui/login/ResetPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.bisamerchant.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.bisamerchant.R
+
+class ResetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
