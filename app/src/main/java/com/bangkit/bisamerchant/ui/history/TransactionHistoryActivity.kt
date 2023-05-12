7b4fe11ca0ebb07584361a4a2547ec9cd4b76b67
tegarfn <tegarfitrah.na@gmail.com>
2023-05-12 23:34:29 +0800

    Create Transaction History Screen - UI

---
+++ app/src/main/java/com/bangkit/bisamerchant/ui/history/TransactionHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.bisamerchant.ui.history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.bisamerchant.R
+
+class TransactionHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transaction_history)
+    }
+}
