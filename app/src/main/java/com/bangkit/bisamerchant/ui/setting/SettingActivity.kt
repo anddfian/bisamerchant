0b7e7c428d28a318de737fa64706bdebf3a06b19
anddfian <andd.fian@gmail.com>
2023-05-13 00:44:17 +0800

    Update SettingActivity.kt

--- app/src/main/java/com/bangkit/bisamerchant/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/bangkit/bisamerchant/ui/setting/SettingActivity.kt
@@ -2,11 +2,36 @@ package com.bangkit.bisamerchant.ui.setting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.bangkit.bisamerchant.R
+import com.bangkit.bisamerchant.databinding.ActivitySettingBinding
+import com.bangkit.bisamerchant.services.Auth
 
-class SettingActivity : AppCompatActivity() {
+class SettingActivity : AppCompatActivity(), View.OnClickListener {
+    private var _binding: ActivitySettingBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        _binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupClickListeners()
     }
-}
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_logout -> {
+                Auth.logout(this, this@SettingActivity)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun setupClickListeners() {
+        binding.btnLogout.setOnClickListener(this)
+    }}
