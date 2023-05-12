3bcff249a6cf62ee512c347c25b02b5000c12812
tegarfn <tegarfitrah.na@gmail.com>
2023-05-13 01:36:44 +0800

    Create Detail Transaction Screen - UI

---
+++ app/src/main/java/com/bangkit/bisamerchant/ui/invoice/DetailTransactionActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.bisamerchant.ui.invoice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.bisamerchant.R
+
+class DetailTransactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_transaction)
+    }
+}
