c1c60991ac47ded229f4eb09b66af9d84a9924ae
tegarfn <tegarfitrah.na@gmail.com>
2023-05-16 20:24:38 +0800

    Create Settings Screen - UI (change)

--- app/src/main/java/com/bangkit/bisamerchant/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/bangkit/bisamerchant/ui/setting/SettingActivity.kt
@@ -1,13 +1,15 @@
 package com.bangkit.bisamerchant.ui.setting
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.bangkit.bisamerchant.R
 import com.bangkit.bisamerchant.databinding.ActivitySettingBinding
 import com.bangkit.bisamerchant.services.Auth
 import com.bangkit.bisamerchant.ui.faq.FaqActivity
+import com.bangkit.bisamerchant.ui.termpolicy.PrivacyPolicyActivity
+import com.bangkit.bisamerchant.ui.termpolicy.TermConditionActivity
 
 class SettingActivity : AppCompatActivity(), View.OnClickListener {
     private var _binding: ActivitySettingBinding? = null
@@ -23,13 +25,31 @@ class SettingActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View) {
         when (v.id) {
+            R.id.cv_setting1 -> {
+                startActivity(Intent(this@SettingActivity, MerchantSettingActivity::class.java))
+            }
+
+            R.id.cv_setting2 -> {
+                startActivity(Intent(this@SettingActivity, TermConditionActivity::class.java))
+            }
+
+            R.id.cv_setting3 -> {
+                startActivity(Intent(this@SettingActivity, PrivacyPolicyActivity::class.java))
+            }
+
+            R.id.cv_setting4 -> {
+                startActivity(Intent(this@SettingActivity, FaqActivity::class.java))
+            }
+
+            R.id.cv_setting5 -> {
+                startActivity(Intent(this@SettingActivity, FaqActivity::class.java))
+            }
+
             R.id.btn_logout -> {
                 Auth.logout(this, this@SettingActivity)
             }
 
-            R.id.btn_faq -> {
-                startActivity(Intent(this@SettingActivity, FaqActivity::class.java))
-            }
+
         }
     }
 
@@ -40,6 +60,10 @@ class SettingActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun setupClickListeners() {
         binding.btnLogout.setOnClickListener(this)
-        binding.btnFaq.setOnClickListener(this)
+        binding.cvSetting1.setOnClickListener(this)
+        binding.cvSetting2.setOnClickListener(this)
+        binding.cvSetting3.setOnClickListener(this)
+        binding.cvSetting4.setOnClickListener(this)
+        binding.cvSetting5.setOnClickListener(this)
     }
 }
