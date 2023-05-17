c00f8e139627de8abbc0aa4bf38ad171a000c185
anddfian <andd.fian@gmail.com>
2023-05-17 23:06:07 +0800

    Update MerchantSettingActivity.kt

--- app/src/main/java/com/bangkit/bisamerchant/ui/setting/MerchantSettingActivity.kt
+++ app/src/main/java/com/bangkit/bisamerchant/ui/setting/MerchantSettingActivity.kt
@@ -2,13 +2,11 @@ package com.bangkit.bisamerchant.ui.setting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatDelegate
 import com.bangkit.bisamerchant.R
 
 class MerchantSettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_merchant_setting)
     }
 }
