040427071cc4dc5ec7b40807765468198e35ed14
Andi Alfian Bahtiar <55624363+anddfian@users.noreply.github.com>
2023-05-19 15:23:14 +0800

    [QR-035] Create Merchant Setting Screen - Logic (#41)
    * Update AESUtil.kt
    
    * Update Owner.kt
    
    * Update activity_setting.xml
    
    * Update SettingActivity.kt
    
    * Update SettingActivity.kt
    
    * Update MerchantRepository.kt
    
    * Update MerchantViewModel.kt
    
    * Update SettingActivity.kt
    
    * Update activity_merchant_setting.xml
    
    * Update SettingActivity.kt
    
    * Update activity_merchant_setting.xml
    
    * Update activity_merchant_setting.xml
    
    * Update activity_profile.xml
    
    * Update build.gradle
    
    * Create ic_loading_24.xml
    
    * Delete image_frame.xml
    
    * Update Merchant.kt
    
    * Update MerchantRegisterActivity.kt
    
    * Update MerchantSettingActivity.kt
    
    * Delete placeholder.png
    
    * Create ProfileActivity.kt
    
    * Update strings.xml
    
    * Update strings.xml
    
    * Update activity_merchant_setting.xml
    
    * Update Merchant.kt

--- app/src/main/java/com/bangkit/bisamerchant/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/bangkit/bisamerchant/ui/setting/SettingActivity.kt
@@ -1,26 +1,41 @@
 package com.bangkit.bisamerchant.ui.setting
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.bangkit.bisamerchant.R
 import com.bangkit.bisamerchant.databinding.ActivitySettingBinding
+import com.bangkit.bisamerchant.helper.MerchantPreferences
+import com.bangkit.bisamerchant.helper.ViewModelMerchantFactory
 import com.bangkit.bisamerchant.services.Auth
 import com.bangkit.bisamerchant.ui.faq.FaqActivity
+import com.bangkit.bisamerchant.ui.home.MerchantViewModel
 import com.bangkit.bisamerchant.ui.termpolicy.PrivacyPolicyActivity
 import com.bangkit.bisamerchant.ui.termpolicy.TermConditionActivity
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("merchant")
 
 class SettingActivity : AppCompatActivity(), View.OnClickListener {
     private var _binding: ActivitySettingBinding? = null
     private val binding get() = _binding!!
+    private var _merchantViewModel: MerchantViewModel? = null
+    private val merchantViewModel get() = _merchantViewModel!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initTopAppBar()
         setupClickListeners()
+        _merchantViewModel = initMerchantViewModel()
     }
 
     override fun onClick(v: View) {
@@ -28,34 +43,48 @@ class SettingActivity : AppCompatActivity(), View.OnClickListener {
             R.id.cv_setting1 -> {
                 startActivity(Intent(this@SettingActivity, MerchantSettingActivity::class.java))
             }
-
             R.id.cv_setting2 -> {
                 startActivity(Intent(this@SettingActivity, TermConditionActivity::class.java))
             }
-
             R.id.cv_setting3 -> {
                 startActivity(Intent(this@SettingActivity, PrivacyPolicyActivity::class.java))
             }
-
             R.id.cv_setting4 -> {
                 startActivity(Intent(this@SettingActivity, FaqActivity::class.java))
             }
-
             R.id.cv_setting5 -> {
-                startActivity(Intent(this@SettingActivity, FaqActivity::class.java))
+                TODO("QR-039")
             }
-
             R.id.btn_logout -> {
+                merchantViewModel.deleteMerchant()
                 Auth.logout(this, this@SettingActivity)
             }
+        }
+    }
 
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+        _merchantViewModel = null
+    }
+
+    private fun initTopAppBar() {
+        setSupportActionBar(binding.topAppBar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowTitleEnabled(true)
+            title = "Setting"
+        }
     }
 
     private fun setupClickListeners() {
@@ -65,5 +94,12 @@ class SettingActivity : AppCompatActivity(), View.OnClickListener {
         binding.cvSetting3.setOnClickListener(this)
         binding.cvSetting4.setOnClickListener(this)
         binding.cvSetting5.setOnClickListener(this)
+    }
+
+    private fun initMerchantViewModel(): MerchantViewModel {
+        val pref = MerchantPreferences.getInstance(dataStore)
+        val factory = ViewModelMerchantFactory.getInstance(pref)
+        val merchantViewModel: MerchantViewModel by viewModels { factory }
+        return merchantViewModel
     }
 }
