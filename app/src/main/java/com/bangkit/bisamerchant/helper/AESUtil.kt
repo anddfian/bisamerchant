cb5b16186c78b86830eb4d6860a95039af17f4f9
Andi Alfian Bahtiar <55624363+anddfian@users.noreply.github.com>
2023-05-19 21:42:35 +0800

    [QR-036] Create PIN Screen - Logic (#43)
    * Update activity_pin.xml
    
    * Update AESUtil.kt
    
    * Update gradle.xml
    
    * Update Merchant.kt
    
    * Update Owner.kt
    
    * Update PinActivity.kt
    
    * Update strings.xml
    
    * Update Transaction.kt

--- app/src/main/java/com/bangkit/bisamerchant/helper/AESUtil.kt
+++ app/src/main/java/com/bangkit/bisamerchant/helper/AESUtil.kt
@@ -1,44 +1,34 @@
 package com.bangkit.bisamerchant.helper
 
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.annotation.RequiresApi
 import javax.crypto.Cipher
-import javax.crypto.SecretKey
-import javax.crypto.SecretKeyFactory
-import javax.crypto.spec.PBEKeySpec
 import javax.crypto.spec.SecretKeySpec
-import android.util.Base64
-import java.security.SecureRandom
+import java.util.Base64
 
 object AESUtil {
-    private const val ALGORITHM = "AES"
-    private const val TRANSFORMATION = "AES/CBC/PKCS7Padding"
-    private const val ENCRYPTION_KEY = "YOUR_ENCRYPTION_KEY" // Replace with your encryption key
-    private const val ITERATION_COUNT = 10000
-    private const val KEY_LENGTH = 256
 
-    fun encrypt(text: String): String {
-        val secretKey: SecretKey = generateSecretKey()
-        val cipher = Cipher.getInstance(TRANSFORMATION)
+    @SuppressLint("GetInstance")
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun encrypt(plainText: String): String {
+        val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+        val key = "0123456789abcdef"
+        val secretKey = SecretKeySpec(key.toByteArray(), "AES")
         cipher.init(Cipher.ENCRYPT_MODE, secretKey)
-        val encryptedBytes = cipher.doFinal(text.toByteArray())
-        return Base64.encodeToString(encryptedBytes, Base64.DEFAULT)
+        val encryptedBytes = cipher.doFinal(plainText.toByteArray())
+        return Base64.getEncoder().encodeToString(encryptedBytes)
     }
 
+    @SuppressLint("GetInstance")
+    @RequiresApi(Build.VERSION_CODES.O)
     fun decrypt(encryptedText: String): String {
-        val secretKey: SecretKey = generateSecretKey()
-        val cipher = Cipher.getInstance(TRANSFORMATION)
+        val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+        val key = "0123456789abcdef"
+        val secretKey = SecretKeySpec(key.toByteArray(), "AES")
         cipher.init(Cipher.DECRYPT_MODE, secretKey)
-        val decodedBytes = Base64.decode(encryptedText, Base64.DEFAULT)
+        val decodedBytes = Base64.getDecoder().decode(encryptedText)
         val decryptedBytes = cipher.doFinal(decodedBytes)
         return String(decryptedBytes)
     }
-
-    private fun generateSecretKey(): SecretKey {
-        val salt = ByteArray(16)
-        SecureRandom().nextBytes(salt)
-        val secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256")
-        val spec = PBEKeySpec(ENCRYPTION_KEY.toCharArray(), salt, ITERATION_COUNT, KEY_LENGTH)
-        val secretKeyBytes = secretKeyFactory.generateSecret(spec).encoded
-        return SecretKeySpec(secretKeyBytes, ALGORITHM)
-    }
 }
-
---
+++ app/src/main/java/com/bangkit/bisamerchant/services/Transaction.kt
@@ -0,0 +1,34 @@
+package com.bangkit.bisamerchant.services
+
+import android.app.Activity
+import android.content.Context
+import android.widget.Toast
+import com.bangkit.bisamerchant.helper.Utils
+import com.google.firebase.firestore.FirebaseFirestore
+
+object Transaction {
+    fun addTransaction(activity: Activity, context: Context, merchantId: String, amount: Long, bankAccountNo: Long, bankInst: String) {
+        val transactionCollection = FirebaseFirestore.getInstance().collection("transaction")
+        val timestampToday = Utils.getTodayTimestamp()
+        val data = hashMapOf(
+            "amount" to amount,
+            "bankAccountNo" to bankAccountNo,
+            "bankInst" to bankInst,
+            "merchantId" to merchantId,
+            "trxType" to "MERCHANT_WITHDRAW",
+            "timestamp" to timestampToday
+        )
+
+        transactionCollection.add(data)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(context, "Withdraw successful", Toast.LENGTH_SHORT).show()
+                    activity.finish()
+                }
+            }
+            .addOnFailureListener { error ->
+                Toast.makeText(context, error.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+    }
+
+}
