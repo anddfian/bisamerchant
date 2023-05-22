b10e1a0d31398845e2f012c5c2ce252008ac0063
tegarfn <tegarfitrah.na@gmail.com>
2023-05-22 21:12:23 +0800

    Create Forgot Password Screen - Logic (Revision)

--- app/src/main/java/com/bangkit/bisamerchant/ui/login/ResetPasswordActivity.kt
+++ app/src/main/java/com/bangkit/bisamerchant/ui/login/ResetPasswordActivity.kt
@@ -1,34 +1,51 @@
 package com.bangkit.bisamerchant.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.bangkit.bisamerchant.R
 import com.bangkit.bisamerchant.databinding.ActivityResetPasswordBinding
 import com.bangkit.bisamerchant.services.Auth
 
 class ResetPasswordActivity : AppCompatActivity(), View.OnClickListener {
     private var _binding: ActivityResetPasswordBinding? = null
-    private val binding get() = _binding!!
+    private val binding get() = _binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityResetPasswordBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding?.root)
 
-        binding.btnResetPassword.setOnClickListener(this)
+        binding.apply {
+            this?.btnResetPassword?.setOnClickListener(this@ResetPasswordActivity).also {
+                this?.btnResetPassword?.isEnabled = false
+            }
+        }
+
+        val textfield = binding?.tilLoginEmail?.editText
+
+        textfield?.addTextChangedListener(object : android.text.TextWatcher {
+            override fun afterTextChanged(s: android.text.Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val filled = textfield.text?.isNotEmpty() ?: false
+                binding?.btnResetPassword?.isEnabled = filled
+            }
+        })
     }
 
     override fun onClick(v: View) {
         when (v.id) {
             R.id.btn_reset_password -> {
-                val email = binding.tilLoginEmail.editText?.text.toString()
-                if (email.isNotEmpty()) {
-                    Auth.resetPasswordEmail(this@ResetPasswordActivity, email)
-                } else {
-                    Toast.makeText(this, "Email is required!", Toast.LENGTH_SHORT).show()
-                }
+                val email = binding?.tilLoginEmail?.editText?.text.toString()
+                when {
+                    !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                        binding?.tilLoginEmail?.error = "Format email salah!"
+                    }
 
+                    else -> {
+                        Auth.resetPasswordEmail(this@ResetPasswordActivity, email)
+                    }
+                }
             }
         }
     }
