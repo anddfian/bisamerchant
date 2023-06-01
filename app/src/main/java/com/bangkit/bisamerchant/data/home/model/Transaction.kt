43aad3610cb7c8a8c62e2398b27c64fe9af3e8ab
Risky Kurniawan <riskykrnawan@gmail.com>
2023-06-01 12:13:07 +0800

    [QR-036] Create PIN Screen - Logic (Revision) (#76)
    * PIN Logic Revision

--- app/src/main/java/com/bangkit/bisamerchant/data/home/model/Transaction.kt
+++
@@ -1,8 +0,0 @@
-package com.bangkit.bisamerchant.data.home.model
-
-data class Transaction(
-	val amount: Long,
-	val trxType: String? = null,
-	val id: String? = null,
-	val timestamp: Long? = null
-)
